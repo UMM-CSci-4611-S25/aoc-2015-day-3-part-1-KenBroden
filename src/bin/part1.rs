ce2c03f7993744675333258b69b58795cab6fe47
Ken <brod0268@morris.umn.edu>
2025-02-19 07:25:14 -0600

    Finished part1 of Santa visiting houses AoC

--- src/bin/part1.rs
+++ src/bin/part1.rs
@@ -1,26 +1,27 @@
-use std::fmt::Result;
+use std::{collections::HashSet, hash::Hash, ops::Add, str::FromStr};
 
-
-// use std::collections::HashSet;
 fn main() {
     let input_file_name = "input.txt";
     let contents = std::fs::read_to_string(input_file_name).expect("Failed to read the input file");
     println!("{}", contents.len());
 }
 
+// Framework of a Position struct
 #[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
 pub struct Pos {
     x: i32,
     y: i32,
 }
 
+// Creates a new instance of the Pos struct
 impl Pos {
-    pub fn new(x: i32, y: i32) -> Pos {
+    #[must_use]
+    pub const fn new(x: i32, y: i32) -> Self {
         Pos { x, y }
     }
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
 pub enum Direction {
     North,
     South,
@@ -28,13 +29,28 @@ pub enum Direction {
     West,
 }
 
+// This allows us to add a direction to a position
+impl Add<Direction> for Pos {
+    type Output = Self;
+
+    fn add(self, direction: Direction) -> Self::Output {
+        match direction {
+            Direction::North => Self::new(self.x, self.y + 1),
+            Direction::South => Self::new(self.x, self.y - 1),
+            Direction::East => Self::new(self.x + 1, self.y),
+            Direction::West => Self::new(self.x - 1, self.y),
+        }
+    }
+}
+
 #[derive(Debug, PartialEq, Eq)]
 pub struct IllegalChar(char);
 
+// Actions for the possible input characters
 impl TryFrom<char> for Direction {
     type Error = IllegalChar;
 
-    fn try_from(c:char) -> Result<Self, Self::Error> {
+    fn try_from(c: char) -> Result<Self, Self::Error> {
         Ok(match c {
             '^' => Self::North,
             'v' => Self::South,
@@ -45,58 +61,76 @@ impl TryFrom<char> for Direction {
     }
 }
 
-struct Moves{
+// Creates the framework for the Moves struct
+pub struct Moves {
     moves: Vec<Direction>,
 }
 
+// Defines the actions for the Moves struct
 impl FromStr for Moves {
     type Err = IllegalChar;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let result: Vec<Direction> = s
+        let moves = s
             .chars()
             .map(Direction::try_from)
             .collect::<Result<Vec<Direction>, IllegalChar>>()?;
-        Ok (Moves { moves })
+        Ok(Moves { moves })
     }
 }
 
+// Creates the framework for the VisitedHouses struct
 pub struct VisitedHouses {
     visited_houses: HashSet<Pos>,
     current_position: Pos,
 }
 
+// Implementations for the VisitedHouses struct
 impl VisitedHouses {
+    // Creates a new instance of the VisitedHouses struct
     #[must_use]
-    pub fn new() -> VisitedHouses {
-        VisitedHouses {
-            let initial_position = Pos::new(0, 0);
-            let mut visited_houses: HashSet<Pos> = HashSet::new();
-            visited_houses.insert(initial_position).clone();
+    pub fn new() -> Self {
+        let initial_position = Pos::new(0, 0);
+        let mut visited_houses = HashSet::new();
+        visited_houses.insert(initial_position);
 
+        Self {
+            visited_houses,
+            current_position: initial_position,
         }
     }
 
-    pub fn num_visited_houses(&self) -> i32 {
-        1
+    // Keeps a count of the number of houses visited
+    #[must_use]
+    pub fn num_visited_houses(&self) -> usize {
+        self.visited_houses.len()
     }
 
+    // Keeps track of the current position
+    #[must_use]
     pub fn current_pos(&self) -> Pos {
-        Pos::new(0, 0)
+        self.current_position
     }
 
-    pub fn perform_move()
+    // Updates the current position and adds the new position to the visited houses
+    pub fn perform_move(&mut self, direction: Direction) {
+        let new_position = self.current_position + direction;
+        self.current_position = new_position;
+        self.visited_houses.insert(new_position);
+    }
 
+    // Performs a series of moves
     pub fn perform_moves(&mut self, moves: Moves) {
-        for direction in moves.moves {
-            self.perform_move(direction);
+        for m in moves.moves {
+            self.perform_move(m);
         }
     }
 }
 
+// The base implementation for the VisitedHouses struct
 impl Default for VisitedHouses {
     fn default() -> Self {
-        VisitedHouses::new()
+        Self::new()
     }
 }
 
@@ -106,51 +140,51 @@ mod tests {
 
     #[test]
     fn test_visited_houses_new() {
-        let /* mut */ visited_houses = VisitedHouses::new();
+        let visited_houses = VisitedHouses::new();
         assert_eq!(visited_houses.num_visited_houses(), 1);
-        // assert_eq!(visited_houses.current_pos(), Pos(0, 0));
+        assert_eq!(visited_houses.current_pos(), Pos::new(0, 0));
     }
 
-    // #[test]
-    // fn test_direction_try_from() {
-    //     assert_eq!('^'.try_into(), Ok(Direction::North));
-    //     assert_eq!('v'.try_into(), Ok(Direction::South));
-    //     assert_eq!('<'.try_into(), Ok(Direction::West));
-    //     assert_eq!('>'.try_into(), Ok(Direction::East));
-    //     assert_eq!(Direction::try_from('x'), Err(IllegalDirectionCharacter('x')));
-    // }
+    #[test]
+    fn test_direction_try_from() {
+        assert_eq!('^'.try_into(), Ok(Direction::North));
+        assert_eq!('v'.try_into(), Ok(Direction::South));
+        assert_eq!('<'.try_into(), Ok(Direction::West));
+        assert_eq!('>'.try_into(), Ok(Direction::East));
+        assert_eq!(Direction::try_from('x'), Err(IllegalChar('x')));
+    }
 
-    // #[test]
-    // fn test_move_east() {
-    //     let mut visited_houses = VisitedHouses::new();
-    //     visited_houses.perform_move(Direction::East);
-    //     assert_eq!(visited_houses.num_visited_houses(), 2);
-    //     assert_eq!(visited_houses.current_pos, Pos(1, 0));
-    // }
+    #[test]
+    fn test_move_east() {
+        let mut visited_houses = VisitedHouses::new();
+        visited_houses.perform_move(Direction::East);
+        assert_eq!(visited_houses.num_visited_houses(), 2);
+        assert_eq!(visited_houses.current_pos(), Pos::new(1, 0));
+    }
 
-    // #[test]
-    // fn test_square_moves() {
-    //     let mut visited_houses = VisitedHouses::new();
-    //     let moves = Moves::from_str("^>v<").unwrap();
-    //     visited_houses.perform_moves(moves);
-    //     assert_eq!(visited_houses.num_visited_houses(), 4);
-    //     assert_eq!(visited_houses.current_pos, Pos(0, 0));
-    // }
+    #[test]
+    fn test_square_moves() {
+        let mut visited_houses = VisitedHouses::new();
+        let moves = Moves::from_str("^>v<").unwrap();
+        visited_houses.perform_moves(moves);
+        assert_eq!(visited_houses.num_visited_houses(), 4);
+        assert_eq!(visited_houses.current_pos(), Pos::new(0, 0));
+    }
 
-    // #[test]
-    // fn test_up_down_moves() {
-    //     let mut visited_houses = VisitedHouses::new();
-    //     let moves = Moves::from_str("^v^v^v^v^v").unwrap();
-    //     visited_houses.perform_moves(moves);
-    //     assert_eq!(visited_houses.num_visited_houses(), 2);
-    //     assert_eq!(visited_houses.current_pos, Pos(0, 0));
-    // }
+    #[test]
+    fn test_up_down_moves() {
+        let mut visited_houses = VisitedHouses::new();
+        let moves = Moves::from_str("^v^v^v^v^v").unwrap();
+        visited_houses.perform_moves(moves);
+        assert_eq!(visited_houses.num_visited_houses(), 2);
+        assert_eq!(visited_houses.current_pos(), Pos::new(0, 0));
+    }
 
-    // #[test]
-    // fn test_aoc_input() {
-    //     let mut visited_houses = VisitedHouses::new();
-    //     let moves = Moves::from_str(include_str!("../input.txt")).unwrap();
-    //     visited_houses.perform_moves(moves);
-    //     assert_eq!(visited_houses.num_visited_houses(), 2565);
-    // }
+    #[test]
+    fn test_aoc_input() {
+        let mut visited_houses = VisitedHouses::new();
+        let moves = Moves::from_str(include_str!("../../input.txt").trim()).unwrap();
+        visited_houses.perform_moves(moves);
+        assert_eq!(visited_houses.num_visited_houses(), 2565);
+    }
 }
